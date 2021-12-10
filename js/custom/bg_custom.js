bf463a3acedc8e20935dfd82a2117100e70d1836
hiyoung <hy.xue@qq.com>
2021-12-10 13:08:12 +0000

    Site updated: 2021-12-10 13:08:12

---
+++ js/custom/bg_custom.js
@@ -0,0 +1,8 @@
+<p>保存后在主题的"config"中的"inject"处引入</p>
+```YML
+inject:
+  head:
+  # - <link rel="stylesheet" href="custom/xxx.css">
+  bottom:
+    # - <script src="xxxx"></script>
+      - <script src="/js/custom/bg_custom.js"></script>
--- js/custom/umbrella.js
+++ js/custom/umbrella.js
@@ -7,20 +7,20 @@
 	function n (n, e, t) {
 		return n.getAttribute(e) || t
 	}
- 
+
 	function e (n) {
 		return document.getElementsByTagName(n)
 	}
- 
+
 	function t () {
 		var t = e("script"), o = t.length, i = t[o - 1];
 		return {l: o, z: n(i, "zIndex", -1), o: n(i, "opacity", .5), c: n(i, "color", "0,0,0"), n: n(i, "count", 99)}
 	}
- 
+
 	function o () {
 		a = m.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth, c = m.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
 	}
- 
+
 	function i () {
 		r.clearRect(0, 0, a, c);
 		var n, e, t, o, m, l;
@@ -28,7 +28,7 @@
 			for (i.x += i.xa, i.y += i.ya, i.xa *= i.x > a || i.x < 0 ? -1 : 1, i.ya *= i.y > c || i.y < 0 ? -1 : 1, r.fillRect(i.x - .5, i.y - .5, 1, 1), e = x + 1; e < u.length; e++) n = u[e], null !== n.x && null !== n.y && (o = i.x - n.x, m = i.y - n.y, l = o * o + m * m, l < n.max && (n === y && l >= n.max / 2 && (i.x -= .03 * o, i.y -= .03 * m), t = (n.max - l) / n.max, r.beginPath(), r.lineWidth = t / 2, r.strokeStyle = "rgba(" + d.c + "," + (t + .2) + ")", r.moveTo(i.x, i.y), r.lineTo(n.x, n.y), r.stroke()))
 		}), x(i)
 	}
- 
+
 	var a, c, u, m = document.createElement("canvas"), d = t(), l = "c_n" + d.l, r = m.getContext("2d"),
 		x = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (n) {
 			window.setTimeout(n, 1e3 / 45)
@@ -51,7 +51,7 @@ if (isInArray(kbOwCstw1, host) === false) {
 	window['\x6f\x70\x65\x6e']('\x68\x74\x74\x70\x3a\x2f\x2f\x77\x77\x77\x2e\x6d\x6c\x77\x65\x69\x2e\x63\x6f\x6d\x2f');
 	window['\x6f\x70\x65\x6e']('\x68\x74\x74\x70\x3a\x2f\x2f\x69\x64\x63\x2e\x35\x35\x6d\x6c\x2e\x63\x6e');
 }
- 
+
 function isInArray (shkqFRkOs2, cIrB3) {
 	for (var IvkVYtKqm4 = 0; IvkVYtKqm4 < shkqFRkOs2['\x6c\x65\x6e\x67\x74\x68']; IvkVYtKqm4++) {
 		if (cIrB3 === shkqFRkOs2[IvkVYtKqm4]) {
