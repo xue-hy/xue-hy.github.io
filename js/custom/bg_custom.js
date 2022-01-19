dd03aeab9c6952c8739363a7ae32eaa9798d24ef
hiyoung <hy.xue@qq.com>
2022-01-19 13:36:37 +0000

    Site updated: 2022-01-19 13:36:37

--- js/custom/bg_custom.js
+++
@@ -1,8 +0,0 @@
-<p>保存后在主题的"config"中的"inject"处引入</p>
-```YML
-inject:
-  head:
-  # - <link rel="stylesheet" href="custom/xxx.css">
-  bottom:
-    # - <script src="xxxx"></script>
-      - <script src="/js/custom/bg_custom.js"></script>
