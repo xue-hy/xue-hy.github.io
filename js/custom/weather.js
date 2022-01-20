523e5860a1767c1e13075df83bb95481107c7939
hiyoung <hy.xue@qq.com>
2022-01-20 11:04:24 +0000

    Site updated: 2022-01-20 11:04:23

---
+++ js/custom/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+    "CONFIG": {
+        "modules": "10423",
+        "background": "5",
+        "tmpColor": "var(--font-color)",
+        "tmpSize": "15",
+        "cityColor": "var(--font-color)",
+        "citySize": "15",
+        "aqiColor": "var(--font-color)",
+        "aqiSize": "15",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "10px 10px 10px 10px",
+        "shadow": "0",
+        "language": "auto",
+        "borderRadius": "5",
+        "fixed": "true",
+        "vertical": "top",
+        "horizontal": "left",
+        "left": "170",
+        "top": "12",
+        "key": "d73d137557f34800ad2809fa420cb2a2"
+    }
+}
